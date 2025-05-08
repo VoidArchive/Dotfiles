9d3dbe4482cbc1ad78dc30e3c6084a9cce319355
voidarchive <voidarchived@gmail.com>
2025-05-08 14:05:44 +0545

    nvim and zshrc setup

---
+++ nvim/init.lua
@@ -0,0 +1,13 @@
+vim.opt.clipboard = "unnamedplus" -- clipboard registry
+vim.opt.number = true           -- show line numbers
+vim.opt.relativenumber = true  -- relative line numbers
+vim.opt.mouse = "a"            -- enable mouse support
+vim.opt.expandtab = true       -- convert tabs to spaces
+vim.opt.shiftwidth = 4         -- indent width
+vim.opt.tabstop = 4            -- number of spaces per tab
+vim.opt.smartindent = true     -- auto-indent new lines
+vim.opt.termguicolors = true   -- enable true color
+vim.opt.scrolloff = 8          -- keep cursor 8 lines from screen edge
+vim.opt.signcolumn = "yes"     -- always show the sign column
+
+
