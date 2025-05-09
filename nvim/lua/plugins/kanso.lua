47a1deb86e4c2b6f5b638225c56b8e994fb0e31d
voidarchive <voidarchived@gmail.com>
2025-05-09 13:17:20 +0545

    just install lazyvim

---
+++ nvim/lua/plugins/kanso.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "webhooked/kanso.nvim",
+    lazy = false,
+    priority = 1000,
+  },
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "kanso-zen",
+    },
+  },
+}
