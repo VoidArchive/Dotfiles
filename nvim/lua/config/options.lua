ac2c18477c79eb2d3b3ae5c86ae074175b2d9b02
voidarchive <voidarchived@gmail.com>
2025-05-27 02:11:50 +0545

    theme change

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.opt.colorcolumn = "80"
+vim.opt.textwidth = 80
+vim.opt.formatoptions:append("t")
+vim.api.nvim_set_hl(0, "ColorColumn", { ctermbg = "darkred" })
