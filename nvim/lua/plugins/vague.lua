e274a5f84c5d76ffc9c6ad4c7bc923c669265d49
voidarchive <voidarchived@gmail.com>
2025-06-25 13:15:58 +0545

    color change

--- nvim/lua/plugins/vague.lua
+++ nvim/lua/plugins/vague.lua
@@ -1,7 +1,7 @@
 return {
   "vague2k/vague.nvim",
   config = function()
-    vim.cmd("colorscheme rose-pine")
+    vim.cmd("colorscheme vague")
     -- NOTE: you do not need to call setup if you don't want to.
     require("vague").setup({
       -- optional configuration here
